06816491e174e7849ed9d9bd3ae624b26e7be0fe
zhorzh <zhorzhwins@gmail.com>
2016-01-20 00:25:11 +0200

    Add Guava for the lolz

--- Books/src/main/Book.java
+++ Books/src/main/Book.java
@@ -1,6 +1,13 @@
 package main;
 
-public class Book {
+import com.google.common.base.Charsets;
+import com.google.common.hash.Funnel;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import com.google.common.hash.PrimitiveSink;
+
+public class Book	{
 
 	private String name;
 	private String downloadUrl;
@@ -27,7 +34,25 @@ public class Book {
 		return "Book name: " + name + " URL: " + downloadUrl;
 	}
 	
+	@SuppressWarnings("serial")
+	public Funnel<Book> getBookFunnel() {
+		return new Funnel<Book>() {
+			@Override
+			public void funnel(Book from, PrimitiveSink into) {
+				into
+					.putString(name, Charsets.UTF_8)
+				    .putString(downloadUrl, Charsets.UTF_8);
+				
+			}
+		};
+	}
+	
+	public int hash(){
+		HashFunction hf = Hashing.md5();
+		HashCode hc = hf.newHasher()
+						.putString(name, Charsets.UTF_8)
+			    		.putString(downloadUrl, Charsets.UTF_8)
+			    		.hash();
+		return hc.asInt();		
+	}
 }
-//comment
-//this is the fix
-//another fix
